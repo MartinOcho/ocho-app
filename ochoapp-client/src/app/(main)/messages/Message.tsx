5324b1893047e48299089778fd31d8d859c42acd
MartinOcho <ochomartinkundabo@gmail.com>
2026-02-03 20:03:55 +0100

    bbn

--- ochoapp-client/src/app/(main)/messages/Message.tsx
+++ ochoapp-client/src/app/(main)/messages/Message.tsx
@@ -203,7 +203,7 @@ export const MessageBubbleContent = ({
           borderRadiusClass
         )}
       >
-        <div className="pr-6 pb-1">
+        <div className="pr-3 pb-1">
             {message.content ? (
                 <HighlightText
                 text={message.content}
