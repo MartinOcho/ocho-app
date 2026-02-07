e688099c92ae0e78d132ac6dbf5f1c38a3d71ef2
MartinOcho <ochomartinkundabo@gmail.com>
2026-02-07 02:38:52 +0100

    fix: ajustement de la largeur maximale du conteneur dans MediaStrip

--- ochoapp-client/src/components/messages/MediaStrip.tsx
+++ ochoapp-client/src/components/messages/MediaStrip.tsx
@@ -25,7 +25,7 @@ export default function MediaStrip({
     <>
       <div
         className={cn(
-          "flex gap-2 flex-wrap mt-2 max-sm:max-w-52",
+          "flex gap-2 flex-wrap mt-2 max-sm:max-w-64",
           className
         )}
       >
