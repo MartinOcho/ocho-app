e1dadc8e3f6aba92df38f873b6967dd136801397
MartinOcho <ochomartinkundabo@gmail.com>
2026-02-07 11:58:14 +0100

    fix: ajustement des classes CSS pour la gestion de l'état fullscreen dans Message et Messages

--- ochoapp-client/src/app/(main)/messages/Message.tsx
+++ ochoapp-client/src/app/(main)/messages/Message.tsx
@@ -742,7 +742,7 @@ export default function Message({
                 </span>
               )}
 
-              <div className={cn("group/message relative w-fit max-w-[75%] select-none flex flex-col", isOwner ? "items-end" : "items-start", isMediaFullscreen && "fixed w-screen h-screen max-sm:translate-x-full")}>
+              <div className={cn("group/message relative w-fit max-w-[75%] select-none flex flex-col", isOwner ? "items-end" : "items-start", isMediaFullscreen && "opacity-0 pointer-events-none")}>
                 
                 {message.senderId !== loggedUser.id && isFirstInCluster && room.isGroup && (
                   <div className="ps-2 text-xs font-semibold text-muted-foreground/80 mb-1 ml-1">
