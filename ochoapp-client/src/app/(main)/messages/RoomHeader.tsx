6926fc01da5e9b7fea5e609d4a77610e48faf4ff
MartinOcho <ochomartinkundabo@gmail.com>
2026-02-08 22:57:53 +0100

    fix: désactiver l'état actif lors du changement de salle

--- ochoapp-client/src/app/(main)/messages/RoomHeader.tsx
+++ ochoapp-client/src/app/(main)/messages/RoomHeader.tsx
@@ -120,7 +120,7 @@ export default function RoomHeader({
   const { socket } = useSocket();
 
   useEffect(() => {
-    backHandler();
+    setActive(false);
   }, [activeRoomId]);
   useEffect(() => {
     if (roomId) {
