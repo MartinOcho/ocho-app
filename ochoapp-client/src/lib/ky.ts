cc6210bdecdd3f52e206866350213cad1f65418d
MartinOcho <ochomartinkundabo@gmail.com>
2026-02-17 21:37:01 +0100

    Test google fix 1

--- ochoapp-client/src/lib/ky.ts
+++ ochoapp-client/src/lib/ky.ts
@@ -2,7 +2,6 @@ import ky from "ky"
 
 
 const kyInstance = ky.create({
-    credentials: "include", // Envoyer les cookies avec chaque requête
     parseJson: (text) => JSON.parse(text, (key, value) => {
         if (key.endsWith("At")) {
             return new Date(value)
