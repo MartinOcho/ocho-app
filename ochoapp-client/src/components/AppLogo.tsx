2c05b556c29d5e7c24713f1a61146b9d941376ec
MartinOcho <ochomartinkundabo@gmail.com>
2026-04-03 13:16:07 +0100

    Desktop ok

--- ochoapp-client/src/components/AppLogo.tsx
+++ ochoapp-client/src/components/AppLogo.tsx
@@ -40,7 +40,7 @@ export default function AppLogo({
   return (
     <span className={className}>
       <div
-        className={`flex select-none items-center ${size <= 80 ? `h-[${size}px]` : `w-[${size}px] flex-col`} gap-1 text-primary`}
+        className={`flex select-none items-center ${size > 80 && `flex-col`} gap-1`}
       >
         {logoEl}
         <span
@@ -80,7 +80,7 @@ function Logo({ className = "", size = 48 }: AppLogoProps) {
         <path
           d="M50,50 C30,30 20,10 50,10 C80,10 70,30 50,50 C30,70 20,90 50,90 C80,90 70,70 50,50 Z"
           fill="none"
-          strokeWidth="14"
+          strokeWidth="8"
           strokeLinecap="round"
           strokeLinejoin="round"
           transform="translate(0,2)"
@@ -102,7 +102,7 @@ function LogoXmas({ className = "", size = 48 }: AppLogoProps) {
     <svg
       width={iconSize/2}
       height={iconSize}
-      viewBox="0 0 100 100"
+      viewBox="0 0 14 24"
       fill="none"
       className={className}
       aria-label="OchoApp logo Noël"
@@ -120,22 +120,14 @@ function LogoXmas({ className = "", size = 48 }: AppLogoProps) {
           <stop offset="1" stopColor="#0c50cc" />
         </linearGradient>
       </defs>
-      <path
-        d="M50,50c-8.8,-8.8 -13.2,-17.6 0,-17.6S58.8,41.2 50,50s-13.2,17.6 0,17.6S58.8,58.8 50,50z"
-        fill="none"
-        stroke="url(#xmasGradient)"
-        strokeWidth="6"
-        strokeLinecap="round"
-        strokeLinejoin="round"
-      />
-      <path
-        d="M48,26.7c0,0 10.8,-8.6 15.5,-5.6c2.5,1.5 2.1,4.2 4.5,15.1c0.5,2.2 -3.6,-2.7 -4.4,-4.2c0,0 -0.3,5.5 -1.2,5.8C60.8,38.1 46.3,28.3 48,26.7z"
-        fill="#FF0000"
-      />
-      <path
-        d="M62.8,39.3c0.1,-0.4 0.2,-0.7 -0.2,-2c-0.6,-1 -1.2,-1.6 -1.7,-2c-0.2,-0.2 1,0.3 0.8,0.2c-1.9,-2.1 -4.4,-3.2 -4.4,-3.2l1.1,-0.1c-1.3,-1.2 -4.1,-2.7 -5.5,-3.2l1.2,-0.1c-1.8,-1.2 -4.2,-2 -5.7,-2.1c-0.2,0 0.2,-0.3 0.2,-0.3l-1,-0.2c-1.1,-0.1 -1.7,0.1 -2.1,1.2l-0.8,1.8c-0.3,0.5 0.1,1.4 1.1,1.4c0,0 2.1,0.6 7.5,4.1c5.2,3.4 6.3,5.9 6.3,5.9c0.7,1 1.4,0.7 2.1,-0.2"
-        fill="#FFFFFF"
-      />
+      <path fill="none" stroke="url(#xmasGradient)" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" stroke-miterlimit="1.4" d="
+	M6.3,11.3c-3.1-3.1-4.6-6.2,0-6.2S9.4,8.2,6.3,11.3s-4.6,6.2,0,6.2S9.4,14.4,6.3,11.3z"/>
+<path fill="#FF0000" d="M5.6,3.1c0,0,3.8-3,5.4-2c0.9,0.5,0.7,1.5,1.6,5.3c0.2,0.8-1.3-0.9-1.5-1.5c0,0-0.1,1.9-0.4,2
+	C10.1,7.1,5,3.7,5.6,3.1z"/>
+<path fill="#FFFFFF" stroke="#00000011" strokeWidth={1} d="M10.8,7.6c0-0.1,0.1-0.2-0.1-0.7c-0.2-0.3-0.4-0.6-0.6-0.7c-0.1-0.1,0.4,0.1,0.3,0.1
+	C9.8,5.5,8.9,5.1,8.9,5.1l0.4,0C8.8,4.6,7.8,4.1,7.4,3.9l0.4,0c-0.6-0.4-1.5-0.7-2-0.7c-0.1,0,0.1-0.1,0.1-0.1L5.5,3
+	C5.1,3,4.9,3,4.8,3.4L4.5,4.1c-0.1,0.2,0,0.5,0.4,0.5c0,0,0.7,0.2,2.6,1.4C9.3,7.2,9.7,8,9.7,8c0.2,0.3,0.5,0.2,0.7-0.1"/>
+    
     </svg>
   );
 }			
