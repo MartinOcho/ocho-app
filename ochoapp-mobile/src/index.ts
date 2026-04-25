187afbc76e3b493b8b452fbce74a55a11ed46a7a
MartinOcho <ochomartinkundabo@gmail.com>
2026-04-25 17:41:19 +0100

    update mobile version

--- ochoapp-mobile/src/index.ts
+++ ochoapp-mobile/src/index.ts
@@ -334,10 +334,10 @@ app.get("/api/activity/searches", getUserSearches);
 app.get("/api/check-update", (req: Request, res: Response) => {
   const version = (req.query.version || "").toString();
   const platform = (req.query.platform || "").toString();
-  const androidCurrentVersion = 1;
-  const androidVersionName = "0.1.002";
-  const iosCurrentVersion = 1;
-  const iosVersionName = "0.1.002";
+  const androidCurrentVersion = 2;
+  const androidVersionName = "0.1.003";
+  const iosCurrentVersion = 2;
+  const iosVersionName = "0.1.003";
   let isUpToDate = true;
   if (platform.toLowerCase() === "android") {
     isUpToDate = parseInt(version) >= androidCurrentVersion;
