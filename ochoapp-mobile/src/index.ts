44a015f9a3e6bdbf38f152f7098d5b8b82789d90
MartinOcho <ochomartinkundabo@gmail.com>
2026-06-30 16:46:58 +0100

    update version numbers for Android and iOS in check-update endpoint

--- ochoapp-mobile/src/index.ts
+++ ochoapp-mobile/src/index.ts
@@ -564,10 +564,10 @@ app.delete("/api/voice-notes/:voiceNoteId", deleteVoiceNote);
 app.get("/api/check-update", (req: Request, res: Response) => {
   const version = (req.query.version || "").toString();
   const platform = (req.query.platform || "").toString();
-  const androidCurrentVersion = 8;
-  const androidVersionName = "0.1.013";
-  const iosCurrentVersion = 8;
-  const iosVersionName = "0.1.013";
+  const androidCurrentVersion = 9;
+  const androidVersionName = "0.1.014";
+  const iosCurrentVersion = 9;
+  const iosVersionName = "0.1.014";
   let isUpToDate = true;
   if (platform.toLowerCase() === "android") {
     isUpToDate = parseInt(version) >= androidCurrentVersion;
