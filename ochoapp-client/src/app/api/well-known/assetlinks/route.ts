1e678285dc00372486af9392cbbfeead44939ae6
MartinOcho <ochomartinkundabo@gmail.com>
2026-08-02 18:51:27 +0100

    add debug signature

--- ochoapp-client/src/app/api/well-known/assetlinks/route.ts
+++ ochoapp-client/src/app/api/well-known/assetlinks/route.ts
@@ -9,6 +9,7 @@ export async function GET() {
         package_name: "com.ochokom.ochoapp",
         sha256_cert_fingerprints: [
           "DB:80:0F:2B:3B:EF:71:0E:51:CF:3F:22:BA:AA:F7:0C:3F:41:5F:DE:CC:4C:00:6F:79:A4:DD:A7:95:85:C7:E7",
+          "93:12:8B:F3:73:2E:34:C8:BB:8A:94:CE:E7:61:64:DA:25:BE:B3:81:9F:D3:73:7C:00:23:83:1D:F8:64:F0:21"
         ],
       },
     },
