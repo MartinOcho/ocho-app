263c4f45f6d1e4170c9e70532084294f215c2538
MartinOcho <ochomartinkundabo@gmail.com>
2026-08-16 02:38:49 +0200

    feat: mettre à jour les versions Android et iOS dans l'endpoint de vérification des mises à jour

--- ochoapp-mobile/src/index.ts
+++ ochoapp-mobile/src/index.ts
@@ -572,10 +572,10 @@ app.delete("/api/voice-notes/:voiceNoteId", deleteVoiceNote);
 app.get("/api/check-update", (req: Request, res: Response) => {
   const version = (req.query.version || "").toString();
   const platform = (req.query.platform || "").toString();
-  const androidCurrentVersion = 12;
-  const androidVersionName = "0.1.017";
-  const iosCurrentVersion = 12;
-  const iosVersionName = "0.1.017";
+  const androidCurrentVersion = 13;
+  const androidVersionName = "0.1.018";
+  const iosCurrentVersion = 13;
+  const iosVersionName = "0.1.018";
   let isUpToDate = true;
   if (platform.toLowerCase() === "android") {
     isUpToDate = parseInt(version) >= androidCurrentVersion;
